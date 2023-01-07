5d755fd15770809ecdf90687b57c4d2e865c7f59
MarcosRubi <danielhernandez9980@gmail.com>
2023-01-07 07:57:26 -0600

    Creando contexto

--- src/context/MainContext.jsx
+++ src/context/MainContext.jsx
@@ -1,9 +1,36 @@
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
+import dataCertificates from "../data/certificates.json";
+import dataProjects from "../data/projects.json";
 
 export const MainContext = createContext();
 
 export function MainContextProvider(props) {
-	return <MainContext.Provider>{props.children}</MainContext.Provider>;
+	const [certificates, setCertificates] = useState([]);
+	const [projects, setProjects] = useState([]);
+
+	useEffect(() => {
+		setCertificates(dataCertificates);
+		setProjects(dataProjects);
+	}, []);
+
+
+	//Devolvemos el proyecto que coincida con la búsqueda
+	const findProjectByTitle = (title) => {
+		return projects.filter((project) => resetTitle(project.title) === title);
+	};
+
+	//Sustituimos espacios en blanco por un guión y todo el título sea en minúsculas
+	const resetTitle = (title) => {
+		return title.split(" ").join("-").toLoweCase();
+	};
+
+	return (
+		<MainContext.Provider
+			value={{ certificates, projects, findProjectByTitle }}
+		>
+			{props.children}
+		</MainContext.Provider>
+	);
 }
 
 export default MainContext;
